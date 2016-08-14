6349fd7cc1e08a67f89de6b481567f6cf7abc524
Disha Virk <dishavirk@Dishas-Air.home>
2016-08-15 01:15:15 +0200

    First Todo App using .Net Core 1.0

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using todoapp.Models;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace todoapp.Controllers
