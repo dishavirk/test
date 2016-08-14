39b953b278b8275d814c659f41e4e38d21b5e64a
Disha Virk <dishavirk@Dishas-Air.home>
2016-08-15 01:35:00 +0200

    test

--- Models/IToDoRepository.cs
+++ Models/IToDoRepository.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
 
 namespace todoapp.Models
-{
-    public interface IToDoRepository
+{public interface IToDoRepository
     {
         void Add (ToDoItem item);
         IEnumerable<ToDoItem> GetAll();
